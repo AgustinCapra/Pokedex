be09d3fb5ecca930c9c4575c5e4151f836602261
Agustin Capra <agustincapra2@gmail.com>
2022-06-17 21:05:42 +0200

    alertas sweet alert

--- main.js
+++ main.js
@@ -1,10 +1,59 @@
 let pokemonIngresado = document.getElementById("pokemonIngresado");
-pokemonIngresado.addEventListener("submit",validarPokemon);
+pokemonIngresado.addEventListener("submit", validarPokemon);
 
-let nombrePokemon = document.getElementById("nombrePokemon");
+let pokemon_nombre = document.getElementById("pokemon_nombre");
+let pokemon_imagen = document.getElementById("pokemon_imagen");
 
-function validarPokemon(e){
+//* API
+
+const pokemon_api = fetch("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0").then((respuesta) => respuesta.json())
+
+function validarPokemon(e) {
     e.preventDefault();
-    console.log(nombrePokemon.value)
+    pokemon_imagen.innerHTML= "";
     
+
+    pokemon_api.then((data) => {
+        const pokemons = data.results;
+        // for (i = 0; i < pokemons.length; i++) {
+        //     console.log(pokemons[i].name)
+        // }
+
+
+
+        const busqueda = pokemons.find(el => 
+            el.name === (pokemon_nombre.value).toLowerCase()
+        )
+
+        if (busqueda == undefined){
+            console.log("no existe ese Pokemon")
+            Swal.fire({
+                title: 'Ey Entrenador!',
+                text: 'Parece que no escribiste un Pokemon valido.',
+                imageUrl: './profesorOak.png',
+                imageWidth: 320,
+                imageHeight: 200,
+                confirmButtonText: 'Volver a intentar',
+                backdrop: `rgba(0,0,123,0.4) url("./pikachu.gif") left center no-repeat`
+              })
+              document.getElementById("pokemon_nombre").value = '';
+        }
+        else{
+            // console.log(busqueda.url)
+            
+            fetch(busqueda.url).then((respuesta) => respuesta.json()).then((data) =>{
+                let imagenPokemon = document.createElement("img");
+                imagenPokemon.setAttribute("src", data.sprites["front_default"]) 
+                imagenPokemon.setAttribute("id", "imagenInsertada");
+                
+                pokemon_imagen.appendChild(imagenPokemon)
+                
+            
+
+            })
+            document.getElementById("pokemon_nombre").value = '';
+        }
+
+    }
+    )
 }
